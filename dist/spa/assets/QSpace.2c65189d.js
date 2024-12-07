c456d87609dd130323b28d780063ab5a826cce3e
marlou <gb.bakeshop.app@gmail.com>
2024-12-07 16:16:12 +0800

    ready for prod

---
+++ dist/spa/assets/QSpace.2c65189d.js
@@ -0,0 +1,1 @@
+import{j as a,f as e}from"./index.f4b8fb6f.js";const s=a("div",{class:"q-space"});var c=e({name:"QSpace",setup(){return()=>s}});export{c as Q};
---
+++ dist/spa/assets/use-quasar.a7ea7bbf.js
@@ -0,0 +1,1 @@
+import{i as a,Y as r}from"./index.f4b8fb6f.js";function u(){return a(r)}export{u};
