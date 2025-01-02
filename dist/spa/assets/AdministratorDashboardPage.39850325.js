d2a5a4b0882cf7cc6f42ca265000dfb3d3326cdc
marlou <gb.bakeshop.app@gmail.com>
2025-01-02 11:49:15 +0800

    ready for the dry run

--- dist/spa/assets/AdministratorDashboardPage.39850325.js
+++
@@ -1,1 +0,0 @@
-import{Q as a}from"./QPage.4bcb1ce6.js";import{o as e,e as t,w as r,$ as o}from"./index.c079c05e.js";const p={__name:"AdministratorDashboardPage",setup(s){return(c,_)=>(e(),t(a,{padding:""},{default:r(()=>[o(" Dashboard Page ")]),_:1}))}};export{p as default};
