0ae44f6bea9f1e9b5cd9fab59a1d25c066b03a24
Marlou Dev <marlou.dev.app@gmail.com>
2025-05-09 16:54:25 +0800

    spa

--- src/pages/login/LoginPage.vue
+++ src/pages/login/LoginPage.vue
@@ -98,7 +98,7 @@ import axios, { api } from "src/boot/axios";
 //   "91c61eefafeaedb3e9cf16507aa391148fab7d8a42c155e4c51d0a1bedb9d12c"
 // ); //SA
 // 631cee3486ce71af // null
-const uuid = ref("b76baeef9c2aef9a"); // branch id 6 endrina
+// const uuid = ref("b76baeef9c2aef9a"); // branch id 6 endrina
 // const uuid = ref("2c1d4509090802cb"); // branch id 1 endrina
 // const uuid = ref(
 //   "91c61eefafeaedb3e9cf16507aa391148fab7d8a42c155e4c51d0a1bedb9d12c"
@@ -113,24 +113,24 @@ const uuid = ref("b76baeef9c2aef9a"); // branch id 6 endrina
 // comment this out //
 // ===================================================//
 
-// const getUUID = async () => {
-//   if (window.require) {
-//     try {
-//       // Import `node-machine-id` using `window.require`
-//       const { machineId } = window.require("node-machine-id");
+const getUUID = async () => {
+  if (window.require) {
+    try {
+      // Import `node-machine-id` using `window.require`
+      const { machineId } = window.require("node-machine-id");
 
-//       // Fetch UUID using `machineId` function
-//       uuid.value = await machineId();
-//     } catch (error) {
-//       console.error("Error fetching machine UUID:", error);
-//     }
-//   } else {
-//     console.warn("Electron not available.");
-//   }
-// };
-// onMounted(() => {
-//   getUUID();
-// });
+      // Fetch UUID using `machineId` function
+      uuid.value = await machineId();
+    } catch (error) {
+      console.error("Error fetching machine UUID:", error);
+    }
+  } else {
+    console.warn("Electron not available.");
+  }
+};
+onMounted(() => {
+  getUUID();
+});
 
 // ===================================================//
 // Use this code for building electron and //
@@ -141,7 +141,7 @@ const uuid = ref("b76baeef9c2aef9a"); // branch id 6 endrina
 const isPwd = ref(true);
 const email = ref("");
 const password = ref("");
-// const uuid = ref("");
+const uuid = ref("");
 const loading = ref(false);
 
 const formIsValid = computed(() => email.value !== "" && password.value !== "");
