3c9c4227487ecaba76da57dad7ecd950171e15e4
Marlou Dev <limgchurch712@gmail.com>
2025-10-22 16:30:07 +0800

    spa

---
+++ dist/spa/assets/AdministratorDashboardPage.1cc2e2e7.js
@@ -0,0 +1,1 @@
+import{Q as a}from"./QPage.57dd675b.js";import{o as e,e as t,w as r,a0 as o}from"./index.4dee972f.js";const p={__name:"AdministratorDashboardPage",setup(s){return(c,_)=>(e(),t(a,{padding:""},{default:r(()=>[o(" Dashboard Page ")]),_:1}))}};export{p as default};
