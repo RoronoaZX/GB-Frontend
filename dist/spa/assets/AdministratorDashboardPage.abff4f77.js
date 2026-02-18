4b91d3ecfca24eb732e67d856aa3cb42871fecff
Marlou Dev <limgchurch712@gmail.com>
2026-02-18 11:56:05 +0800

    spa

---
+++ dist/spa/assets/AdministratorDashboardPage.abff4f77.js
@@ -0,0 +1,1 @@
+import{Q as a}from"./QPage.c874f623.js";import{o as e,e as t,w as r,a0 as o}from"./index.86a43405.js";const p={__name:"AdministratorDashboardPage",setup(s){return(c,_)=>(e(),t(a,{padding:""},{default:r(()=>[o(" Dashboard Page ")]),_:1}))}};export{p as default};
---
+++ dist/spa/assets/use-quasar.4a2498e2.js
@@ -0,0 +1,1 @@
+import{i as a,Y as r}from"./index.86a43405.js";function u(){return a(r)}export{u};
